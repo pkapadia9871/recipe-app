07f19c6c8fb385055dd73349063a87ca0a64999d
pkapadia9871 <30789273+pkapadia9871@users.noreply.github.com>
2023-11-22 16:39:24 -0600

    Delete src directory

--- src/recipes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import recipes_home
-
-app_name = 'recipes'
-
-urlpatterns = [
-   path('', recipes_home),
-]
--- src/recipes/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def recipes_home(request):
-   return render(request, 'recipes/recipes_home.html')
